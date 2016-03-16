c28599cc7d00b259fbb448a96e4f09d0d0fcd4f2
MuresanVladut <muresan25@gmail.com>
2016-03-16 15:36:28 +0200

    Add MVC6 to project.

---
+++ src/ASP.NET/Controllers/Web/AppController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET.Services;
+using ASP.NET.ViewModel;
+using Microsoft.AspNet.Mvc;
+
+namespace ASP.NET.Controllers.Web
+{
+    public class AppController :Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ClientViewModelcs model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("","Could not send email");
+                }
+                if (_mailService.SendMail(email, email, $"Contact Page from {model.Name} ({model.Email})", model.Message))
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = "Mail Sent!";
+                }
+                
+            }
+            return View();
+        }
+    }
+}
--- src/ASP.NET/Startup.cs
+++ src/ASP.NET/Startup.cs
@@ -2,41 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NET.Services;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace ASP.NET
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddScoped<IMailService, DebugMailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-//            app.UseIISPlatformHandler();
-
-           /* app.Run(async (context) =>
-            {
-                var html = @"<!DOCTYPE html>
-                        <html>
-                        <head>
-                        </head>
-                        <body>
-                            <h2>HelloWorld</h2>
-                        </body>
-                        </html>";
-                           
-                await context.Response.WriteAsync(html);
-            });*/
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "App", action = "Index"}
+                    );
+            });
         }
 
         // Entry point for the application.
